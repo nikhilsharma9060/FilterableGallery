bbcd92a7dc2debe5ed9310685324ac05e15c3620
nikhilsharma9060 <84852875+nikhilsharma9060@users.noreply.github.com>
2023-08-04 12:07:36 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,214 @@
+const categories = [
+    {
+        name:'All',
+        filter:'all',
+    },
+    {
+        name:'Arms',
+        filter:'arms',
+    },
+    {
+        name:'Cars',
+        filter:'cars',
+    },
+    {
+        name:'Dark',
+        filter:'dark',
+    },
+    {
+        name:'Leaf',
+        filter:'leaf',
+    },
+    {
+        name:'Nature',
+        filter:'nature',
+    },
+    {
+        name:'Rajasthan',
+        filter:'rajasthan',
+    },
+];
+
+const images = [
+    {
+        id:'1',
+        url: './Arms/a1.jpg',
+        category: 'arms',
+        title:'Arm 1',
+    },
+    {
+        id:'2',
+        url: './Arms/a2.jpg',
+        category: 'arms',
+        title:'Arm 2',
+    },
+    {
+        id:'3',
+        url: './Arms/a3.jpg',
+        category: 'arms',
+        title:'Arm 3',
+    },
+    {
+        id:'4',
+        url: './Arms/a4.jpg',
+        category: 'arms',
+        title:'Arm 4',
+    },
+    {
+        id:'5',
+        url: './Arms/a5.jpg',
+        category: 'arms',
+        title:'Arm 5',
+    },
+    {
+        id:'6',
+        url: './Cars/c1.jpg',
+        category: 'cars',
+        title:'Car 1',
+    },
+    {
+        id:'7',
+        url: './Cars/c2.jpg',
+        category: 'cars',
+        title:'Car 2',
+    },
+    {
+        id:'8',
+        url: './Cars/c3.jpg',
+        category: 'cars',
+        title:'Car 3',
+    },
+    {
+        id:'9',
+        url: './Cars/c4.jpg',
+        category: 'cars',
+        title:'Car 4',
+    },
+    {
+        id:'10',
+        url: './Cars/c5.jpg',
+        category: 'cars',
+        title:'Car 5',
+    },
+    {
+        id:'11',
+        url: './Dark/d1.jpg',
+        category: 'dark',
+        title:'Dark 1',
+    },
+    {
+        id:'12',
+        url: './Dark/d2.jpg',
+        category: 'dark',
+        title:'Dark 2',
+    },
+    {
+        id:'13',
+        url: './Dark/d3.jpg',
+        category: 'dark',
+        title:'Dark 3',
+    },
+    {
+        id:'14',
+        url: './Dark/d4.jpg',
+        category: 'dark',
+        title:'Dark 4',
+    },
+    {
+        id:'15',
+        url: './Dark/d5.jpg',
+        category: 'dark',
+        title:'Dark 5',
+    },
+    {
+        id:'16',
+        url: './Leaf/w1.jpg',
+        category: 'leaf',
+        title:'Leaf 1',
+    },
+    {
+        id:'17',
+        url: './Leaf/w2.jpg',
+        category: 'leaf',
+        title:'Leaf 2',
+    },
+    {
+        id:'18',
+        url: './Leaf/w3.jpg',
+        category: 'leaf',
+        title:'Leaf 3',
+    },
+    {
+        id:'19',
+        url: './Leaf/w4.jpg',
+        category: 'leaf',
+        title:'Leaf 4',
+    },
+    {
+        id:'20',
+        url: './Leaf/w5.jpg',
+        category: 'leaf',
+        title:'Leaf 5',
+    },
+    {
+        id:'21',
+        url: './Nature/n1.jpg',
+        category: 'nature',
+        title:'Nature 1',
+    },
+    {
+        id:'22',
+        url: './Nature/n2.jpg',
+        category: 'nature',
+        title:'Nature 2',
+    },
+    {
+        id:'23',
+        url: './Nature/n3.jpg',
+        category: 'nature',
+        title:'Nature 3',
+    },
+    {
+        id:'24',
+        url: './Nature/n4.jpg',
+        category: 'nature',
+        title:'Nature 4',
+    },
+    {
+        id:'25',
+        url: './Nature/n5.jpg',
+        category: 'nature',
+        title:'Nature 5',
+    },
+    {
+        id:'26',
+        url: './Raj/r1.jpg',
+        category: 'rajasthan',
+        title:'Raj 1',
+    },
+    {
+        id:'27',
+        url: './Raj/r2.jpg',
+        category: 'rajasthan',
+        title:'Raj 2',
+    },
+    {
+        id:'28',
+        url: './Raj/r3.jpg',
+        category: 'rajasthan',
+        title:'Raj 3',
+    },
+    {
+        id:'29',
+        url: './Raj/r4.jpg',
+        category: 'rajasthan',
+        title:'Raj 4',
+    },
+    {
+        id:'30',
+        url: './Raj/r5.jpg',
+        category: 'rajasthan',
+        title:'Raj 5',
+    }
+
+]
